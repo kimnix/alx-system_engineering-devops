a1d26b50429bd5a2d3f4f51e7f931b1c41b2f158
kimnix <kimnix@gmail.com>
2024-08-15 11:35:04 -0700

    subscribers for reddit subreddit

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
-
-"""
-Script that queries subscribers on a given Reddit subreddit.
-"""
-
+"""Function to query subscribers on a given reddit subreddit."""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
+    """Return total no of subscribers."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"}
     response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 200:
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-    else:
+    if response.status_code == 404:
         return 0
-
-
-print(number_of_subscribers("python"))  # Should return the number of subscribers in the 'python' subreddit
-print(number_of_subscribers("nonexistentsubreddit"))  # Should return 
-
+    results = response.json().get("data")
+    return results.get("subscribers")
 
